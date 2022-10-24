c156d80cafeec1911317e88645347c1552afb2ac
Maryam <113088482+m-ry-m@users.noreply.github.com>
2022-10-24 09:10:17 +0400

    Update Exercize2.py

--- Chapter 2- Variables & Comments/Exercises/Exercize2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercize2.py
@@ -1,7 +1,34 @@
-#Exercize2
+#Exercize1
 Fame=str('Nice to meet you')
 print(Fame)
 Fame=str('You can do it, Keep going')
 print(Fame)
-#Exercize3
+#Exercize2
+print('Jalal ud din Muhammad Rumi once said,\"Whatever you lost through fate, be certain that it saved you from pain\"')
+#Exercize3 Stripping names
+name = "\tSaad\n"
+
+print("Unmodified:")
+print(name)
+
+print("Using lstrip():")
+print(name.lstrip())
+
+print("Using rstrip():")
+print(name.rstrip())
+
+print("Using strip():")
+print(name.strip())
+#Exercize4 Favorite number
+num=8
+mg=f"Destiny number might be {num}"
+print(mg)
+
+#Exercize5 USB Shopper
+
+money=50
+usb_cost=6
+print("The girl buys 8 USB sticks")
+mon_left=50-6*8
+print("Money she's left after byuing 8 USB sticks:",mon_left)
 
