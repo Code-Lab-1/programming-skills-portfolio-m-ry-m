2b348bec937fce66129ca4d1fae971a97bafb7ad
Maryam <113088482+m-ry-m@users.noreply.github.com>
2022-10-26 10:04:02 +0400

    Create Exercize2.py

---
+++ Chapter 3- Data Structures/Exercize2.py
@@ -0,0 +1,12 @@
+names=['Sidra','Nour','Rumaisa','Tayyaba']
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+
+print('Dear',names[0],"Always be happy ;)")
+print('Yo',names[1],'Be strong, That\'s what make you who you are')
+print('HI',names[2],'Distance is only a number')
+print('Hello',names[3],'Time makes me wish for my past')
