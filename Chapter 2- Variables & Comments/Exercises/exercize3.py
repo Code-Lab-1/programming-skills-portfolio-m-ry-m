a3ffabfd82080bf51a4c6aefda67cd69d7682a82
Maryam <113088482+m-ry-m@users.noreply.github.com>
2022-10-26 20:24:21 +0400

    Create exercize3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercize3.py
@@ -0,0 +1,14 @@
+#Exercize3 Stripping names
+name = "\tSaad\n"
+
+print("Unmodified:")
+print(name)
+
+print("Using lstrip():")
+print(name.lstrip())
+
+print("Using rstrip():")
+print(name.rstrip())
+
+print("Using strip():")
+print(name.strip())
