6f755b427c6811fe31296e6bd296bf8f7117ff85
Maryam <113088482+m-ry-m@users.noreply.github.com>
2022-10-26 20:40:58 +0400

    Update Exercize2.py

--- Chapter 2- Variables & Comments/Exercises/Exercize2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercize2.py
@@ -1,11 +1,5 @@
 #Exercize2
 print('Jalal ud din Muhammad Rumi once said,\"Whatever you lost through fate, be certain that it saved you from pain\"')
 
-#Exercize5 USB Shopper
 
-money=50
-usb_cost=6
-print("The girl buys 8 USB sticks")
-mon_left=50-6*8
-print("Money she's left after byuing 8 USB sticks:",mon_left)
 
