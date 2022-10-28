41655bb5d3d16b0101f7d018f42090d21e7cdee1
Maryam <113088482+m-ry-m@users.noreply.github.com>
2022-10-28 23:30:41 +0400

    Update Exercize3.py

---
+++ Chapter 5- Dictionaries/Exercize3.py
@@ -0,0 +1,11 @@
+glossary = {
+    'dictionary': "A collection of key-value pairs.",'string': 'A series of characters.',
+    'float': 'A numerical value with a decimal component.',
+    'comment': 'A note in a program that the Python interpreter ignores.',
+    'list': 'A collection of items in a particular order.',
+    'loop': 'Itering over a sequence',
+    'key': 'The first item in a key-value pair in a dictionary.',
+    'value': 'An item associated with a key in a dictionary.',
+    'conditional test': 'A comparison between two values.',
+    'boolean ': 'represents one of two values, True or False',
+    }
