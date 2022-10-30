a48a2899de5a181a238acc4939eef5ba24cff397
Maryam <113088482+m-ry-m@users.noreply.github.com>
2022-10-30 17:50:16 +0400

    Update Exercize4.py

--- Chapter 5- Dictionaries/Exercize4.py
+++ Chapter 5- Dictionaries/Exercize4.py
@@ -2,9 +2,9 @@
 river={'Congo':'Africa','Volga':'Russia','Amur':'China'}
 for i,j in river.items():
     print('\n'+i+":"+j)
-print('Rivers in dictionary')
+print('\nRivers in dictionary')
 for i in river.keys():
     print('\n'+i)
-print("Countries in dicctionary")
+print("\nCountries in dicctionary")
 for i in river.values():
     print('\n'+i)
