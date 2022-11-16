f4573e60add90c5c8a891854147752093a41d980
Maryam <113088482+m-ry-m@users.noreply.github.com>
2022-11-16 11:05:18 +0400

    Update Exercize9.py

---
+++ Chapter 3- Data Structures/Exercises/Exercize9.py
@@ -0,0 +1,6 @@
+#Comment why you don't like these subjects
+subjects=['English','Computer','Physics','Chemistry']
+print('Subjects:',subjects) 
+subjects.pop()
+subjects.remove('Physics')
+print("Favorite subjects: ",subjects)
